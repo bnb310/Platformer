2b033d56341f8e03d70f4affc80608576ec8b399
Nicki Pitcher <50596030+bnb310@users.noreply.github.com>
2019-07-09 11:08:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,39 +2,22 @@
 
 const apiKey = '2093ca02909677c3ebd944675cf48e98e29c8f5d'; 
 
-const gameURL = `https://www.giantbomb.com/api/games/?api_key=${apiKey}&format=jsonp`;
-
 let gameGUID = 0;
 
-let nextList = 0;
-
-function findGUID(gameInput) {
+function findGUID (gameInput) {
   $.ajax({
       type: 'get',
-      url: `https://www.giantbomb.com/api/games/?api_key=${apiKey}&format=jsonp&field_list=name,guid&offset=${nextList}`,
+      url: `https://www.giantbomb.com/api/search/?api_key=${apiKey}&format=jsonp&query=${gameInput}&resources=game`,
       dataType: 'jsonp',
       jsonp: 'json_callback',
       success: function(data) {
         console.log(data);
-      for (let i = 0; i <= data.results.length; i++) {
-        /*let returnedName = data.results[i].name;*/
-      if (gameInput == data.results[i].name) {
-        gameGUID = data.results[i].guid;
-        $(findPlatforms(gameGUID));
-        break;
-      }
-      /*else if (i = data.results.length) {
-        nextList += 100;
-        $(findGUID);
-      }*/
-    }
+        gameGUID = data.results[0].guid;
+        findPlatforms(gameGUID);
       },
     });
-
-    /*https://stackoverflow.com/questions/3963906/problems-executing-a-jquery-ajax-call-within-a-function*/
-
-    /*https://www.w3schools.com/jsref/jsref_break.asp*/
 }
+
 
 function findPlatforms(gameGUID) {
   $.ajax({
@@ -44,15 +27,17 @@ function findPlatforms(gameGUID) {
       jsonp: 'json_callback',
       success: function(data) {
         console.log(data);
-        $(displayResults);
+        displayResults(data);
       },
     });
 }
 
 function displayResults(data) {
   $('.results').empty();
+  $('.results').append(`<h2>${data.results.name} is available on:</h2>`);
   for (let i = 0; i < data.results.platforms.length; i++) {
-  $('.results').append(`<li>${data.results.platforms[i].platform.name}</li>`)}
+    console.log(data.results.platforms[i]);
+  $('.results').append(`<li>${data.results.platforms[i].name}</li>`)}
 }
 
 function watchForm() {
